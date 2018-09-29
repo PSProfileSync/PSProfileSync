2c3c062d19d58e5d1d08712da618ccb8fe8209b1
Constantin Hager <constantin.hager@outlook.de>
2018-09-29 11:59:45 +0200

    Initial Commit of plater Template

---
+++ PSProfileSync/PSProfileSync.settings.ps1
@@ -0,0 +1,56 @@
+# Settings for build.
+param(
+    $Artifacts = './artifacts',
+    $Packages = './packages',
+    $ModuleName = 'PSProfileSync',
+    $ModulePath = '.\PSProfileSync',
+    $Lib = "$ModulePath\Lib",
+    $BuildNumber = $env:BUILD_BUILDID,
+    $PercentCompliance = '50'
+)
+
+########################################################################
+# Static settings.
+########################################################################
+$Settings = @{
+#    SMBRepositoryName = ''
+#    SMBRepositoryPath = ''
+#
+#    Author = "<author>"
+#    Owners = ""
+#    LicenseUrl = ""
+#    ProjectUrl = ""
+#    PackageDescription = ""
+#    Repository = ""
+#    Tags = ""
+#
+#    GitRepo = ""
+#    CIUrl = ""
+#    ApiKey = ""
+}
+
+
+########################################################################
+# Before / Hooks
+########################################################################
+
+#Synopsis: Executues before the Clean Task.
+#task BeforeClean {}
+#Synopsis: Executues after the Clean Task.
+#task AfterClean {}
+
+#Synopsis: Executues before the Analyze Task.
+#task BeforeAnalyze {}
+#Synopsis: Executues after the Analyze Task.
+#task AfterAnalyze {}
+
+#Synopsis: Executes before Test Task.
+#task BeforeTest {}
+#Synopsis: Executes after Test Task.
+#task AfterTest {}
+
+#Synopsis: Executes before Publish Task.
+#task BeforePublish {}
+#Synopsis: Executes after Publish Task.
+#task AfterPublish {}
+
---
+++ PSProfileSync/Tests/PSProfileSync.Tests.ps1
@@ -0,0 +1,10 @@
+$ModuleManifestName = 'PSProfileSync.psd1'
+$ModuleManifestPath = "$PSScriptRoot\..\PSProfileSync\$ModuleManifestName"
+
+Describe 'Module Manifest Tests' {
+    It 'Passes Test-ModuleManifest' {
+        Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
+        $? | Should Be $true
+    }
+}
+
