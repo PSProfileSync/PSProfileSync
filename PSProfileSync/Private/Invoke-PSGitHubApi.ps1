e4b123b19defcfde1e2a2fcbfed34b0eff6dcce5
Constantin Hager <constantin.hager@outlook.de>
2018-10-03 10:30:47 +0200

    Write help for the command.

--- PSProfileSync/Private/Invoke-PSGitHubApi.ps1
+++ PSProfileSync/Private/Invoke-PSGitHubApi.ps1
@@ -1,5 +1,35 @@
 function Invoke-PSGitHubApi
 {
+    <#
+    .SYNOPSIS
+        Call the GitHub Api
+    .DESCRIPTION
+        Call the GitHub Api to Get / Add information.
+
+    .PARAMETER PersonalAccessToken
+    The personal accesstoken of the user
+
+    .PARAMETER UserName
+    the username of the user
+
+    .PARAMETER Method
+    The method. This can be Get / Post / Patch
+
+    .PARAMETER Uri
+    the Uri of the GitHub Api that you want to call
+
+    .EXAMPLE
+        PS C:\> Invoke-PsGitHubApi -UserName testuser -PersonalAccessToken 00000 -Method Get -Uri https://api.github.com/user
+        Authenticate user testuser with his PAT of 00000 to the github Api and gets his userinformation back.
+
+    .OUTPUTS
+        JSon-Object
+    .NOTES
+        Author: Constantin Hager, Johannes Kuemmel
+        Date: 03.10.2018
+    #>
+
+
     [CmdletBinding()]
     param (
         # the PAT
