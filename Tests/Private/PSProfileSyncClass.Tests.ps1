89f60e39fa786ebf7f2fa220f3ec4d2fe17823b8
Constantin Hager <constantin.hager@outlook.de>
2018-10-14 14:13:22 +0200

    Write tests for the following methods: GetPSRepository SavePSRepositoriesToFile ConverttoZipArchive ExecuteEncodeCertUtil ExecuteDecodeCertUtil

--- Tests/Private/PSProfileSyncClass.Tests.ps1
+++ Tests/Private/PSProfileSyncClass.Tests.ps1
@@ -160,5 +160,63 @@ InModuleScope PSProfileSync {
                 $returnvalue.PATToken | Should -BeExactly $PatToken
             }
         }
+
+        Context "GetPSRepository" {
+            It "Gets an object back" {
+                $obj = New-MockObject -Type System.Object
+                Mock -CommandName "Get-PSRepository" -MockWith {
+                    return $obj
+                }
+
+                $returnvalue = $PSProfileSyncClass.GetPSRepository()
+                $returnvalue | Should -BeOfType System.Object
+                Assert-MockCalled -CommandName "Get-PSRepository" -Exactly 1
+            }
+        }
+
+        Context "SavePSRepositoriesToFile" {
+
+            Mock -CommandName "ConvertTo-Json" -MockWith {}
+            Mock -CommandName "Out-File" -MockWith {}
+
+            It "Method returns no errors" {
+                $returnvalue = $PSProfileSyncClass.SavePSRepositoriesToFile()
+                { $returnvalue } | Should -Not -Throw
+                Assert-MockCalled -CommandName ConvertTo-Json -Exactly 1
+            }
+        }
+
+        Context "ConverttoZipArchive" {
+            It "Converts the files to a zip archive" {
+                $Sourcepath = "Sourcepath"
+                $Targetpath = "Targetpath"
+                Mock -CommandName "Compress-Archive" -MockWith {}
+                $returnvalue = $PSProfileSyncClass.ConverttoZipArchive($Sourcepath, $Targetpath)
+                {$returnvalue} | Should -Not -Throw
+                Assert-MockCalled -CommandName "Compress-Archive" -Exactly 1
+            }
+        }
+
+        Context "ExecuteEncodeCertUtil" {
+            It "Encodes a file to a certificate" {
+                $Sourcepath = "Sourcepath"
+                $Targetpath = "Targetpath"
+                Mock -CommandName "Start-Process" -MockWith {}
+                $returnvalue = $PSProfileSyncClass.ExecuteEncodeCertUtil($Sourcepath, $Targetpath)
+                {$returnvalue} | Should -Not -Throw
+                Assert-MockCalled -CommandName "Start-Process" -Exactly 1
+            }
+        }
+
+        Context "ExecuteDecodeCertUtil" {
+            It "Decodes a file back to the original" {
+                $Sourcepath = "Sourcepath"
+                $Targetpath = "Targetpath"
+                Mock -CommandName "Start-Process" -MockWith {}
+                $returnvalue = $PSProfileSyncClass.ExecuteDecodeCertUtil($Sourcepath, $Targetpath)
+                {$returnvalue} | Should -Not -Throw
+                Assert-MockCalled -CommandName "Start-Process" -Exactly 1
+            }
+        }
     }
 }
