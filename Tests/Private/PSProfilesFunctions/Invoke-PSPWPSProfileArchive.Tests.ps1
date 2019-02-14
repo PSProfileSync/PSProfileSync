a3e5ee8018b186d295cb7f755194204bb3379aa8
Constantin Hager <constantin.hager@outlook.de>
2019-02-14 21:45:33 +0100

    Added test files for Profile functions

---
+++ Tests/Private/PSProfilesFunctions/Invoke-PSPWPSProfileArchive.Tests.ps1
@@ -0,0 +1,17 @@
+$ModuleManifestName = 'PSProfileSync.psd1'
+$Root = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
+$ModuleManifestPath = Join-Path $Root -ChildPath "PSProfileSync\$ModuleManifestName"
+
+if (Get-Module PSProfileSync)
+{
+    Remove-Module PSProfileSync
+    Import-Module $ModuleManifestPath
+}
+else
+{
+    Import-Module $ModuleManifestPath
+}
+
+InModuleScope PSProfileSync {
+
+}
