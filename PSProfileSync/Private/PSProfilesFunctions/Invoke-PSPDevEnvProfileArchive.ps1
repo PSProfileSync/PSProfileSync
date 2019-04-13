2e20ab1259046746838988cfd5323c9a576a9f8a
Constantin Hager <constantin.hager@outlook.de>
2019-04-13 15:10:26 +0200

    added the right variable names to the call

--- PSProfileSync/Private/PSProfilesFunctions/Invoke-PSPDevEnvProfileArchive.ps1
+++ PSProfileSync/Private/PSProfilesFunctions/Invoke-PSPDevEnvProfileArchive.ps1
@@ -3,5 +3,5 @@ function Invoke-PSPDevEnvProfileArchive
     $PSProfileDevEnvArchiveFolderPathZip = Get-PSFConfigValue -FullName "PSProfileSync.profile.profiledevenvarchivefolderpathzip"
     $EncodedPSProfileDevEnvArchiveFolderPathZip = Get-PSFConfigValue -FullName "PSProfileSync.profile.encodedpsprofiledevenvarchivefolderpathzip"
 
-    Invoke-PSPEncodeCertUtil -SourcePath $PSProfilePSCoreArchiveFolderPathZip -TargetPath $EncodedPSProfilePSCoreArchiveFolderPathZip
+    Invoke-PSPEncodeCertUtil -SourcePath $PSProfileDevEnvArchiveFolderPathZip -TargetPath $EncodedPSProfileDevEnvArchiveFolderPathZip
 }
